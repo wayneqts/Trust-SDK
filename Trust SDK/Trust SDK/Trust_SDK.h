404d3feefd7d5bf7b16bfdeca8c26e811af15df5
QTS Coder <tester.qts@gmail.com>
2022-06-22 15:45:52 +0700

    Initial project setup

---
+++ Trust SDK/Trust SDK/Trust_SDK.h
@@ -0,0 +1,18 @@
+//
+//  Trust_SDK.h
+//  Trust SDK
+//
+//  Created by QTS Coder on 22/06/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Trust_SDK.
+FOUNDATION_EXPORT double Trust_SDKVersionNumber;
+
+//! Project version string for Trust_SDK.
+FOUNDATION_EXPORT const unsigned char Trust_SDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Trust_SDK/PublicHeader.h>
+
+
---
+++ Trust SDK/Trust SDK/Trust_SDK.swift
@@ -0,0 +1,8 @@
+//
+//  Trust_SDK.swift
+//  Trust SDK
+//
+//  Created by QTS Coder on 22/06/2022.
+//
+
+import Foundation
