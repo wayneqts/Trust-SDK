2db4a68fa37d2112985d11bfa5dfaacad306f691
QTS Coder <tester.qts@gmail.com>
2022-06-24 08:59:08 +0700

    Update function get device info

--- Trust SDK/Trust SDK/Device.swift
+++ Trust SDK/Trust SDK/Device.swift
@@ -10,88 +10,171 @@ import NotificationCenter
 
 public extension UIDevice
 {
-    //Original Author: HAS
-    // https://stackoverflow.com/questions/26028918/how-to-determine-the-current-iphone-device-model
-    // Modified by Sam Trent
-
+    
     /**********************************************
-    *  getCPUName():
-    *     Returns a hardcoded value of the current
-    * devices CPU name.
-    ***********************************************/
+     *  getCPUName():
+     *     Returns a hardcoded value of the current
+     * devices CPU name.
+     ***********************************************/
     func getCPUName() -> String
     {
         let processorNames = Array(CPUinfo().keys)
         return processorNames[0]
     }
-
+    
     /**********************************************
-    *  getCPUSpeed():
-    *     Returns a hardcoded value of the current
-    * devices CPU speed as specified by Apple.
-    ***********************************************/
-    func getCPUSpeed() -> String
-    {
-        let processorSpeed = Array(CPUinfo().values)
-        return processorSpeed[0]
-    }
-
-    /**********************************************
-    *  CPUinfo:
-    *     Returns a dictionary of the name of the
-    *  current devices processor and speed.
-    ***********************************************/
+     *  CPUinfo:
+     *     Returns a dictionary of the name of the
+     *  current devices processor and speed.
+     ***********************************************/
     private func CPUinfo() -> Dictionary<String, String> {
-
+        
 #if targetEnvironment(simulator)
         let identifier = ProcessInfo().environment["SIMULATOR_MODEL_IDENTIFIER"]!
-        #else
-
+#else
+        
         var systemInfo = utsname()
         uname(&systemInfo)
         let machineMirror = Mirror(reflecting: systemInfo.machine)
         let identifier = machineMirror.children.reduce("") { identifier, element in
-        guard let value = element.value as? Int8 , value != 0 else { return identifier }
-        return identifier + String(UnicodeScalar(UInt8(value)))
-     }
-  #endif
-
-  switch identifier {
-  case "iPod5,1":                                 return ["A5":"800 MHz"] // underclocked
-  case "iPod7,1":                                 return ["A8":"1.4 GHz"]
-  case "iPhone3,1", "iPhone3,2", "iPhone3,3":     return ["A4":"800 MHz"] // underclocked
-  case "iPhone4,1":                               return ["A5":"800 MHz"] // underclocked
-  case "iPhone5,1", "iPhone5,2":                  return ["A6":"1.3 GHz"]
-  case "iPhone5,3", "iPhone5,4":                  return ["A6":"1.3 GHz"]
-  case "iPhone6,1", "iPhone6,2":                  return ["A7":"1.3 GHz"]
-  case "iPhone7,2":                               return ["A8":"1.4 GHz"]
-  case "iPhone7,1":                               return ["A8":"1.4 GHz"]
-  case "iPhone8,1":                               return ["A9":"1.85 GHz"]
-  case "iPhone8,2":                               return ["A9":"1.85 GHz"]
-  case "iPhone9,1", "iPhone9,3":                  return ["A10 Fusion":"2.34 GHz"]
-  case "iPhone9,2", "iPhone9,4":                  return ["A10 Fusion":"2.34 GHz"]
-  case "iPhone8,4":                               return ["A9":"1.85 GHz"]
-  case "iPhone10,1", "iPhone10,4":                return ["A11 Bionic":"2.39 GHz"]
-  case "iPhone10,2", "iPhone10,5":                return ["A11 Bionic":"2.39 GHz"]
-  case "iPhone10,3", "iPhone10,6":                return ["A11 Bionic":"2.39 GHz"]
-  case "iPad2,1", "iPad2,2", "iPad2,3", "iPad2,4":return ["A5":"1.0 GHz"]
-  case "iPad3,1", "iPad3,2", "iPad3,3":           return ["A5X":"1.0 GHz"]
-  case "iPad3,4", "iPad3,5", "iPad3,6":           return ["A6X":"1.4 GHz"]
-  case "iPad4,1", "iPad4,2", "iPad4,3":           return ["A7":"1.4 GHz"]
-  case "iPad5,3", "iPad5,4":                      return ["A8X":"1.5 GHz"]
-  case "iPad6,11", "iPad6,12":                    return ["A9":"1.85 GHz"]
-  case "iPad2,5", "iPad2,6", "iPad2,7":           return ["A5":"1.0 GHz"]
-  case "iPad4,4", "iPad4,5", "iPad4,6":           return ["A7":"1.3 GHz"]
-  case "iPad4,7", "iPad4,8", "iPad4,9":           return ["A7":"1.3 GHz"]
-  case "iPad5,1", "iPad5,2":                      return ["A8":"1.5 GHz"]
-  case "iPad6,3", "iPad6,4":                      return ["A9X":"2.16 GHz"] // underclocked
-  case "iPad6,7", "iPad6,8":                      return ["A9X":"2.24 GHz"]
-  case "iPad7,1", "iPad7,2":                      return ["A10X Fusion":"2.34 GHz"]
-  case "iPad7,3", "iPad7,4":                      return ["A10X Fusion":"2.34 GHz"]
-  case "AppleTV5,3":                              return ["A8":"1.4 GHz"]
-  case "AppleTV6,2":                              return ["A10X Fusion":"2.34 GHz"]
-  case "AudioAccessory1,1":                       return ["A8":"1.4 GHz"] // clock speed is a guess
-  default:                                        return ["N/A":"N/A"]
-  }
+            guard let value = element.value as? Int8 , value != 0 else { return identifier }
+            return identifier + String(UnicodeScalar(UInt8(value)))
+        }
+#endif
+        
+        switch identifier {
+        case "iPod5,1":                                 return ["A5":"800 MHz"] // underclocked
+        case "iPod7,1":                                 return ["A8":"1.4 GHz"]
+        case "iPhone3,1", "iPhone3,2", "iPhone3,3":     return ["A4":"800 MHz"] // underclocked
+        case "iPhone4,1":                               return ["A5":"800 MHz"] // underclocked
+        case "iPhone5,1", "iPhone5,2":                  return ["A6":"1.3 GHz"]
+        case "iPhone5,3", "iPhone5,4":                  return ["A6":"1.3 GHz"]
+        case "iPhone6,1", "iPhone6,2":                  return ["A7":"1.3 GHz"]
+        case "iPhone7,2":                               return ["A8":"1.4 GHz"]
+        case "iPhone7,1":                               return ["A8":"1.4 GHz"]
+        case "iPhone8,1":                               return ["A9":"1.85 GHz"]
+        case "iPhone8,2":                               return ["A9":"1.85 GHz"]
+        case "iPhone9,1", "iPhone9,3":                  return ["A10 Fusion":"2.34 GHz"]
+        case "iPhone9,2", "iPhone9,4":                  return ["A10 Fusion":"2.34 GHz"]
+        case "iPhone8,4":                               return ["A9":"1.85 GHz"]
+        case "iPhone10,1", "iPhone10,4":                return ["A11 Bionic":"2.39 GHz"]
+        case "iPhone10,2", "iPhone10,5":                return ["A11 Bionic":"2.39 GHz"]
+        case "iPhone10,3", "iPhone10,6":                return ["A11 Bionic":"2.39 GHz"]
+        case "iPad2,1", "iPad2,2", "iPad2,3", "iPad2,4":return ["A5":"1.0 GHz"]
+        case "iPad3,1", "iPad3,2", "iPad3,3":           return ["A5X":"1.0 GHz"]
+        case "iPad3,4", "iPad3,5", "iPad3,6":           return ["A6X":"1.4 GHz"]
+        case "iPad4,1", "iPad4,2", "iPad4,3":           return ["A7":"1.4 GHz"]
+        case "iPad5,3", "iPad5,4":                      return ["A8X":"1.5 GHz"]
+        case "iPad6,11", "iPad6,12":                    return ["A9":"1.85 GHz"]
+        case "iPad2,5", "iPad2,6", "iPad2,7":           return ["A5":"1.0 GHz"]
+        case "iPad4,4", "iPad4,5", "iPad4,6":           return ["A7":"1.3 GHz"]
+        case "iPad4,7", "iPad4,8", "iPad4,9":           return ["A7":"1.3 GHz"]
+        case "iPad5,1", "iPad5,2":                      return ["A8":"1.5 GHz"]
+        case "iPad6,3", "iPad6,4":                      return ["A9X":"2.16 GHz"] // underclocked
+        case "iPad6,7", "iPad6,8":                      return ["A9X":"2.24 GHz"]
+        case "iPad7,1", "iPad7,2":                      return ["A10X Fusion":"2.34 GHz"]
+        case "iPad7,3", "iPad7,4":                      return ["A10X Fusion":"2.34 GHz"]
+        case "AppleTV5,3":                              return ["A8":"1.4 GHz"]
+        case "AppleTV6,2":                              return ["A10X Fusion":"2.34 GHz"]
+        case "AudioAccessory1,1":                       return ["A8":"1.4 GHz"] // clock speed is a guess
+        default:                                        return ["N/A":"N/A"]
+        }
+    }
+    
+    var isSimulator: Bool {
+        return TARGET_OS_SIMULATOR != 0
+    }
+    
+    var isJailBroken: Bool {
+        get {
+            if UIDevice.current.isSimulator { return false }
+            if JailBrokenHelper.hasCydiaInstalled() { return true }
+            if JailBrokenHelper.isContainsSuspiciousApps() { return true }
+            if JailBrokenHelper.isSuspiciousSystemPathsExists() { return true }
+            return JailBrokenHelper.canEditSystemFiles()
+        }
+    }
+    
+    func totalDiskSpaceInBytes() -> Int64 {
+        do {
+            guard let totalDiskSpaceInBytes = try FileManager.default.attributesOfFileSystem(forPath: NSHomeDirectory())[FileAttributeKey.systemSize] as? Int64 else {
+                return 0
+            }
+            return totalDiskSpaceInBytes
+        } catch {
+            return 0
+        }
+    }
 }
+private struct JailBrokenHelper {
+    //check if cydia is installed (using URI Scheme)
+    static func hasCydiaInstalled() -> Bool {
+        return UIApplication.shared.canOpenURL(URL(string: "cydia://")!)
+    }
+    
+    //Check if suspicious apps (Cydia, FakeCarrier, Icy etc.) is installed
+    static func isContainsSuspiciousApps() -> Bool {
+        for path in suspiciousAppsPathToCheck {
+            if FileManager.default.fileExists(atPath: path) {
+                return true
+            }
+        }
+        return false
+    }
+    
+    //Check if system contains suspicious files
+    static func isSuspiciousSystemPathsExists() -> Bool {
+        for path in suspiciousSystemPathsToCheck {
+            if FileManager.default.fileExists(atPath: path) {
+                return true
+            }
+        }
+        return false
+    }
+    
+    //Check if app can edit system files
+    static func canEditSystemFiles() -> Bool {
+        let jailBreakText = "Developer Insider"
+        do {
+            try jailBreakText.write(toFile: jailBreakText, atomically: true, encoding: .utf8)
+            return true
+        } catch {
+            return false
+        }
+    }
+    
+    //suspicious apps path to check
+    static var suspiciousAppsPathToCheck: [String] {
+        return ["/Applications/Cydia.app",
+                "/Applications/blackra1n.app",
+                "/Applications/FakeCarrier.app",
+                "/Applications/Icy.app",
+                "/Applications/IntelliScreen.app",
+                "/Applications/MxTube.app",
+                "/Applications/RockApp.app",
+                "/Applications/SBSettings.app",
+                "/Applications/WinterBoard.app"
+        ]
+    }
+    
+    //suspicious system paths to check
+    static var suspiciousSystemPathsToCheck: [String] {
+        return ["/Library/MobileSubstrate/DynamicLibraries/LiveClock.plist",
+                "/Library/MobileSubstrate/DynamicLibraries/Veency.plist",
+                "/private/var/lib/apt",
+                "/private/var/lib/apt/",
+                "/private/var/lib/cydia",
+                "/private/var/mobile/Library/SBSettings/Themes",
+                "/private/var/stash",
+                "/private/var/tmp/cydia.log",
+                "/System/Library/LaunchDaemons/com.ikey.bbot.plist",
+                "/System/Library/LaunchDaemons/com.saurik.Cydia.Startup.plist",
+                "/usr/bin/sshd",
+                "/usr/libexec/sftp-server",
+                "/usr/sbin/sshd",
+                "/etc/apt",
+                "/bin/bash",
+                "/Library/MobileSubstrate/MobileSubstrate.dylib"
+        ]
+    }
 }
--- Trust SDK/Trust SDK/Trust_SDK.swift
+++ Trust SDK/Trust SDK/Trust_SDK.swift
@@ -7,46 +7,235 @@
 
 import Foundation
 import UIKit
+import AdSupport
+import ExternalAccessory
+import SystemConfiguration.CaptiveNetwork
+import MachO
+import CoreNFC
+import AVFoundation
 public final class Trust_SDK {
-
     let name = "Trust_SDK"
-    
     public init(){
         
     }
     public func get_device_info()-> NSDictionary{
-        return ["os": "i", "os_version":self.getOSInfo(), "battery_charging": self.GetbatteryCharging(), "battery_level": self.getBatteryLevel() * 100, "cpu": UIDevice.current.getCPUName(), "free_storage": self.deviceRemainingFreeSpaceInBytes() ?? 0, "gpu_name": "Apple GPU", "gpu_vendor": "Apple Inc", "screen_height": UIScreen.main.bounds.size.height, "screen_width": UIScreen.main.bounds.size.width, "udid": UIDevice.current.identifierForVendor?.uuid ?? ""]
+        return [
+            "aaid": self.identifierForAdvertising() ?? "",
+            "accessories": self.countAccessories(),
+            "adid": self.identifierForAdvertising() ?? "",
+            "pencil": self.isPencialDevice(),
+            "battery_charging": self.getBatteryCharging(),
+            "battery_level": self.getBatteryLevel() ,
+            "bluetooth_mac_address": "-",
+            "camera_megapixel": "-",
+            "cpu": UIDevice.current.getCPUName(),
+            "cpu_cores": self.getCpuCore(),
+            "cpu_type": self.getArch() ?? "",
+            "css_image_loaded": false,
+            "developer_settings": false,
+            "display_zoom": false,
+            "ds_card": false,
+            "eid": "-",
+            "free_storage": self.deviceRemainingFreeSpaceInBytes() ?? 0,
+            "gpu_name": "-",
+            "gpu_vendor": "-",
+            "guided_access": self.isGuidedAccess(),
+            "iccid" : "-",
+            "icloud_ubiquti_token": "-",
+            "identifier": self.getIdentifier(),
+            "imei" :"-",
+            "imsi": "-",
+            "language": self.getLanguage(),
+            "latitude": "-",
+            "longitude": "-",
+            "local_language": self.getLocalLanguage(),
+            "low_power_mode": self.isLowPowerMode(),
+            "name": UIDevice.current.name,
+            "nfc_enabled": self.isEnableNFC(),
+            "os":"i",
+            "os_version": self.getOSInfo(),
+            "physical_memory": ProcessInfo.processInfo.physicalMemory,
+            "ringer_mode": "-",
+            "root_hiders": "-",
+            "rooted": UIDevice.current.isJailBroken,
+            "screen_brightness": UIScreen.main.brightness,
+            "screen_dpi": self.screenPPIAndDPI(),
+            "screen_height": UIScreen.main.bounds.size.height,
+            "screen_ppi": self.screenPPIAndDPI(),
+            "screen_width": UIScreen.main.bounds.size.width,
+            "screen_resolution": self.getScreenResolution(),
+            "screen_touch": "-",
+            "seid": "-",
+            "storage_capacity": UIDevice.current.totalDiskSpaceInBytes(),
+            "total_applications" : "-",
+            "udid": self.getIdentifier(),
+            "unlock_type": "-",
+            "virtual_env": "-",
+            "ip": self.getIPAddress(),
+            "mobile": self.isMobile(),
+            "wifi_bssid": "-",
+            "wifi_ssid": "-",
+            "wifi_security": "-",
+            "wifi_mac_address": "-"
+        ]
     }
     
-    func getOSInfo()->String {
+    private func getScreenResolution()-> String{
+        let width = UIScreen.main.nativeBounds.size.width
+        let height = UIScreen.main.nativeBounds.size.height
+        return "\(width) x \(height)"
+        
+    }
+    
+    private func getOSInfo()->String {
         let os = ProcessInfo.processInfo.operatingSystemVersion
         return String(os.majorVersion) + "." + String(os.minorVersion) + "." + String(os.patchVersion)
     }
     
-    func GetbatteryCharging()-> Bool{
-        if UIDevice.current.batteryState == .unplugged {
-            //Not charging
-            return false
-        } else {
-            //Charging
+    private func getLanguage()-> String{
+        let locale = Locale.current
+        guard let languageCode = locale.languageCode,
+              let regionCode = locale.regionCode else {
+            return ""
+        }
+        return languageCode + "_" + regionCode
+    }
+    
+    private func getLocalLanguage()-> String{
+        let language = Locale.preferredLanguages.first
+        return language ?? ""
+    }
+    
+    
+    private func getBatteryCharging()-> Bool{
+        UIDevice.current.isBatteryMonitoringEnabled = true
+        if UIDevice.current.batteryState == .charging {
             return true
+        } else {
+            return false
         }
     }
     
-    func getBatteryLevel()-> Float{
-        return UIDevice.current.batteryLevel
+    private func getBatteryLevel()-> Float{
+        UIDevice.current.isBatteryMonitoringEnabled = true
+        let batteryLevel = UIDevice.current.batteryLevel * 100
+        return batteryLevel
     }
     
-    func deviceRemainingFreeSpaceInBytes() -> Int64? {
+    private func deviceRemainingFreeSpaceInBytes() -> Int64? {
         let documentDirectory = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).last!
         guard
             let systemAttributes = try? FileManager.default.attributesOfFileSystem(forPath: documentDirectory),
             let freeSize = systemAttributes[.systemFreeSize] as? NSNumber
         else {
-            // something failed
             return nil
         }
         return freeSize.int64Value
+    }
+    
+    
+    private func identifierForAdvertising() -> String? {
+        guard ASIdentifierManager.shared().isAdvertisingTrackingEnabled else {
+            return nil
+        }
+        
+        // Get and return IDFA
+        return ASIdentifierManager.shared().advertisingIdentifier.uuidString
+    }
+    
+    
+    private func countAccessories()-> Int{
+        let accessCount = EAAccessoryManager.shared().connectedAccessories.count
+        return accessCount
+    }
+    
+    
+    // Check pencil for device
+    private func isPencialDevice()->Bool{
+        if UIDevice.current.userInterfaceIdiom == UIUserInterfaceIdiom.pad && UIScreen.main.traitCollection.forceTouchCapability == .available{
+            return true
+        }
+        return false
+    }
+    
+    private func getCpuCore()->Int{
+        return ProcessInfo.processInfo.activeProcessorCount
+    }
+    
+    private func getArch() -> String? {
+        guard let archRaw = NXGetLocalArchInfo().pointee.name else {
+            return nil
+        }
+        return String(cString: archRaw)
+    }
+    
+    private func isGuidedAccess()-> Bool{
+        if UIAccessibility.isGuidedAccessEnabled {
+            return true
+        }  else {
+            return false
+        }
+    }
+    
+    private func getIdentifier()->String{
+        let uuid = UIDevice.current.identifierForVendor?.uuidString
+        return uuid ?? ""
+    }
+    
+    private func isLowPowerMode()-> Bool{
+        if ProcessInfo.processInfo.isLowPowerModeEnabled{
+            return true
+        }
+        return false
+    }
+    
+    private func isEnableNFC()->Bool{
+        if #available(iOS 11.0, *) {
+            if NFCNDEFReaderSession.readingAvailable {
+                return true
+            }
+            else {
+                return false
+            }
+        }
+        else{
+            return false
+        }
+    }
+    
+    private func getIPAddress() -> String {
+        var address: String?
+        var ifaddr: UnsafeMutablePointer<ifaddrs>? = nil
+        if getifaddrs(&ifaddr) == 0 {
+            var ptr = ifaddr
+            while ptr != nil {
+                defer { ptr = ptr?.pointee.ifa_next }
+                
+                guard let interface = ptr?.pointee else { return "" }
+                let addrFamily = interface.ifa_addr.pointee.sa_family
+                if addrFamily == UInt8(AF_INET) || addrFamily == UInt8(AF_INET6) {
+                    let name: String = String(cString: (interface.ifa_name))
+                    if  name == "en0" || name == "en2" || name == "en3" || name == "en4" || name == "pdp_ip0" || name == "pdp_ip1" || name == "pdp_ip2" || name == "pdp_ip3" {
+                        var hostname = [CChar](repeating: 0, count: Int(NI_MAXHOST))
+                        getnameinfo(interface.ifa_addr, socklen_t((interface.ifa_addr.pointee.sa_len)), &hostname, socklen_t(hostname.count), nil, socklen_t(0), NI_NUMERICHOST)
+                        address = String(cString: hostname)
+                    }
+                }
+            }
+            freeifaddrs(ifaddr)
+        }
+        return address ?? ""
+    }
+    
+    private func isMobile()-> Bool{
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return true
+        }
+        return false
+    }
+    
+    private func screenPPIAndDPI()-> Int{
+        return Int(UIScreen.main.nativeScale * (UIDevice.current.userInterfaceIdiom == UIUserInterfaceIdiom.pad ? 132 : 163))
     }
     
 }
